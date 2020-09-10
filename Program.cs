8aac5b68647afada43bad6bf94146426007ed6fe
Sara Martinez <s_martinez_1990@outlook.com>
2020-09-09 22:44:22 -0400

    added user input commands and string parse to #s

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace College_Admission
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your high school grade point average");
+            float userGradePoint = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Now, please enter your admissions test score");
+            float testScore = float.Parse(Console.ReadLine());
         }
     }
 }
